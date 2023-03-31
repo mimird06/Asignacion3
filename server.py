21363507a24bab181226c0a87ed05d7eb943a50e
Thammy F. Vargas A <thammyfva@hotmail.com>
2023-03-30 23:43:27 -0400

    add recetas and database-s data

--- server.py
+++ server.py
@@ -14,5 +14,27 @@ def inicio():
 def agregar():
     return render_template('agregar.html')
 
+@servidor.route('/agregar/datos', methods=['POST'])
+def datos():
+    
+    nombre = request.form['nombre']
+    ingredientes = request.form['ingredientes']
+    url = request.form['link-img']
+
+    contenedor = (nombre, ingredientes, url)
+    cursorDB.execute("INSERT INTO recetas (nombre, ingredientes, link) VALUES (?, ?, ?)", contenedor)
+    db.commit()
+
+    alerta = 'Datos se han guardado correctamente.'
+
+    return render_template('agregar.html', alerta=alerta)
+
+@servidor.route('/recetas')
+def recetas():
+
+    datos = db.execute('SELECT * FROM recetas')
+
+    return render_template('recetas.html', acept=datos)
+
 if __name__=='__main__':
     servidor.run(debug=True, port=6542)
